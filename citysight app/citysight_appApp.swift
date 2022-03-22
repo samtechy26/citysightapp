e53b130ec5722667f98282d3570e1597f101fbd8
samtechy26 <samkan061@gmail.com>
2022-03-22 08:05:07 -0400

    Initial Commit

---
+++ citysight app/citysight_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  citysight_appApp.swift
+//  citysight app
+//
+//  Created by Sam Tech on 3/22/22.
+//
+
+import SwiftUI
+
+@main
+struct citysight_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
